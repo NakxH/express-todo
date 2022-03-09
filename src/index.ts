f81b2613bc50d03e40035b16100abe6311e48d46
Hakan <74737999+NakxH@users.noreply.github.com>
2022-03-09 12:58:03 +0000

    Second Commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
 import bodyParser from 'body-parser';
 import express, {Request, Response} from 'express';
-const app = express();
+import mongoose from 'mongoose';
 const port = 3000; // default port to listen
-app.use(bodyParser.json())
-
-// define a route handler for the default home page
-// app.get( "/", ( req: Request, res: Response ) => {
-//     res.send( "Hello world!" );
-// } );
 
 interface ITodo {
     id: number,
@@ -33,40 +27,54 @@ const todos: ITodo[] = [
     }
 ];
 
-app.post( "/todo", ( req: Request, res: Response ) => {
-    const todo = {
-        id: todos.length + 1,
-        text: req.body.text,
-        completed: false,
-    }
+mongoose
+	.connect("mongodb://localhost:27017/todo_app")
+	.then(() => {
+		const app = express()
 
-    todos.push(todo)
-    res.send( todos );
-} );
+        app.use(bodyParser.json())
 
-app.get( "/todo", ( req: Request, res: Response ) => {
-    res.send( todos );
-} );
+		app.post( "/todo", ( req: Request, res: Response ) => {
+            const todo = {
+                id: todos.length + 1,
+                text: req.body.text,
+                completed: false,
+            }
+        
+            todos.push(todo)
+            res.send( todo );
+        } );
+        
+        app.get( "/todo", ( req: Request, res: Response ) => {
+            res.send( todos );
+        } );
+        
+        app.get( "/todo/:id", ( req: Request, res: Response ) => {
+            const todo = todos.find( (todo) => todo.id === parseInt(req.params.id, 10));
+            res.send( todo );
+        } );
+        
+        app.put( "/todo/:id", ( req: Request, res: Response ) => {
+            const todo = todos.find( (todo) => todo.id === parseInt(req.params.id, 10));
+            todo.text = req.body.text
+            todo.completed = req.body.completed
+            res.send( todo );
+        } );
+        
+        app.delete( "/todo/:id", ( req: Request, res: Response ) => {
+            const todoIndex = todos.findIndex( (todo) => todo.id === parseInt(req.params.id, 10));
+            todos.splice(todoIndex, 1);
+            res.send( todos );
+        } );
+        
+        // start the Express server
+        app.listen( port, () => {
+            console.log( `server started at http://localhost:${ port }` );
+        } );
+	})
 
-app.get( "/todo/:id", ( req: Request, res: Response ) => {
-    const todo = todos.find( (todo) => todo.id === parseInt(req.params.id, 10));
-    res.send( todo );
-} );
+// define a route handler for the default home page
+// app.get( "/", ( req: Request, res: Response ) => {
+//     res.send( "Hello world!" );
+// } );
 
-app.put( "/todo/:id", ( req: Request, res: Response ) => {
-    const todo = todos.find( (todo) => todo.id === parseInt(req.params.id, 10));
-    todo.text = req.body.text
-    todo.completed = req.body.completed
-    res.send( todo );
-} );
-
-app.delete( "/todo/:id", ( req: Request, res: Response ) => {
-    const todoIndex = todos.findIndex( (todo) => todo.id === parseInt(req.params.id, 10));
-    todos.splice(todoIndex, 1);
-    res.send( todos );
-} );
-
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
